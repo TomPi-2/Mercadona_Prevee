a71e7a19526498fc39cd61da1b38443d1c0ca5da
TomPi <filipinas162@gmail.com>
2025-11-10 13:13:00 +0100

    chore: Remove database seeding logic from main function

--- prevee_web/lib/main.dart
+++ prevee_web/lib/main.dart
@@ -12,10 +12,6 @@ Future<void> main() async {
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFwYm92YWVvdnJ2eHhlZ3Bic3V1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3NTAxNDEsImV4cCI6MjA3ODMyNjE0MX0.JWSjalh6lrwk53_web6BhYC6xkEpScdWFDm7qXsQqUw',
   );
 
-  DatabaseSeeder seeder = DatabaseSeeder();
-  await seeder.clearProducts();
-  await seeder.seedProducts();
-
   runApp(const MyApp());
 }
 
